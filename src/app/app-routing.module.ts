1208f5ac02e75076a41f064e0ec04a3de80b2530
Ahmed Reda <48368885+AhmedReda431@users.noreply.github.com>
2019-06-12 16:15:27 +0200

    all source code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
+import { ProductsComponent } from './products/products.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { ShopingCartComponent } from './shoping-cart/shoping-cart.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes =
+[
+  {path: 'products', component : ProductsComponent},
+  {path: 'orders', component : MyOrdersComponent},
+  {path: 'admin/orders', component : AdminOrdersComponent},
+  {path: 'admin/products', component : ProductsComponent},
+  {path: 'shoping-cart', component : ShopingCartComponent},
+  {path: 'login', component : LoginComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ProductsComponent } from './products/products.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { ShopingCartComponent } from './shoping-cart/shoping-cart.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ProductsComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    ShopingCartComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, NgbModule,
     AppRoutingModule
   ],
   providers: [],
